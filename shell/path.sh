1db18d4c65cf240b5e6364b91298a80a302dc5ed
Brian Schiller <bgschiller@gmail.com>
2014-04-09 09:04:43 -0500

    Added GNU coreutils to path

--- shell/path.sh
+++ shell/path.sh
@@ -1,9 +1,13 @@
 
+PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH" #coreutils w/o g prefix
 PATH="/usr/local/bin:$PATH" #homebrew path
 PATH="/usr/local/share/python:$PATH" #pip-installed executables
 PATH="/usr/local/opt/ruby/bin:$PATH" #gem-installed executables
 PATH="$HOME/bin:$PATH"
 export PATH
+
+MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
+export MANPATH
 
 PYTHONPATH=/usr/local/lib/python2.7/site-packages:$PYTHONPATH
 PYTHONPATH=/usr/local/lib/python:$PYTHONPATH
