13d1b840aa7a70570cc8d7c853ea790e18c8f6da
Brian Schiller <bgschiller@gmail.com>
2014-04-24 16:35:38 -0500

    Added Go directories

--- shell/path.sh
+++ shell/path.sh
@@ -1,6 +1,9 @@
+export GOPATH=$HOME/go
 
 PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH" #coreutils w/o g prefix
 PATH="/usr/local/opt/ruby/bin:$PATH" #gem-installed executables
+PATH="/usr/local/Cellar/go/1.2.1/libexec/bin:$PATH" # GOROOT-based install
+PATH="$GOPATH/bin:$PATH"
 PATH="/Applications/Postgres.app/Contents/Versions/9.3/bin:$PATH" #Postgres.app path
 PATH="$HOME/bin:$PATH"
 PATH="/usr/local/bin:$PATH" #homebrew path
@@ -12,6 +15,8 @@ export MANPATH
 PYTHONPATH=/usr/local/lib/python2.7/site-packages:$PYTHONPATH
 PYTHONPATH=/usr/local/lib/python:$PYTHONPATH
 export PYTHONPATH
+
+
 
 source /usr/local/bin/virtualenvwrapper.sh
 
