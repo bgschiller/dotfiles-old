d9f237cf650f8da2e1b28336a28845a7b9316b48
Brian Schiller <bgschiller@gmail.com>
2014-05-21 16:16:17 -0500

    Added serve alias

--- shell/alias.sh
+++ shell/alias.sh
@@ -1,2 +1,6 @@
 # Pipe my public key to my clipboard.
 alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
+
+#serve from local directory
+alias serve="python -m SimpleHTTPServer"
+
