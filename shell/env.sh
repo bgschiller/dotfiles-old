ca36c8f5af72f87dbcd5ffb27fd6983eb19c59ab
Brian Schiller <bgschiller@gmail.com>
2014-12-18 10:01:51 -0600

    add virtualenvwrapper

--- shell/env.sh
+++ shell/env.sh
@@ -10,3 +10,5 @@ export HOMEBREW_GITHUB_API_TOKEN=ca5f5b692f0bc0dad5e8e16e2e271c654204868b
 export GDAL_DRIVER_PATH=/usr/local/lib/gdalplugins
 
 export DISABLE_KEYERROR=1
+
+source /usr/local/bin/virtualenvwrapper.sh
