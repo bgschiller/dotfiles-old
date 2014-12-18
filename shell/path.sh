2c2195da3a3641d0b343a425f840b19308be72a2
Brian Schiller <bgschiller@gmail.com>
2014-12-18 10:21:21 -0600

    update postgres app path

--- shell/path.sh
+++ shell/path.sh
@@ -4,7 +4,7 @@ PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH" #coreutils w/o g prefix
 PATH="/usr/local/opt/ruby/bin:$PATH" #gem-installed executables
 PATH="/usr/local/Cellar/go/1.2.1/libexec/bin:$PATH" # GOROOT-based install
 PATH="$GOPATH/bin:$PATH"
-PATH="/Applications/Postgres.app/Contents/Versions/9.3/bin:$PATH" #Postgres.app path
+PATH="/Applications/Postgres.app/Contents/Versions/9.4/bin:$PATH" #Postgres.app path
 PATH="$HOME/bin:$PATH"
 PATH="/usr/local/bin:$PATH" #homebrew path
 export PATH
